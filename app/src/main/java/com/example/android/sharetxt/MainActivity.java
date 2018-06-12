eab07ef071a2316714514551c73ee20a09414ddc
Abhiraj singh <a906891@gmail.com>
2018-06-13 02:25:01 +0530

    Added a List View Works good And on Clicking Shows position of the clicked list item in logcat

--- app/src/main/java/com/example/android/sharetxt/MainActivity.java
+++ app/src/main/java/com/example/android/sharetxt/MainActivity.java
@@ -1,16 +1,40 @@
 package com.example.android.sharetxt;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ////STRING AS NAME ARRAY //
+        String[] cars = {"hero honda","maruti","me"};
+        /////TO VIEW STRING  NAME IN LISTVIEW////
+        ListView listview = (ListView) findViewById(R.id.listview);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_expandable_list_item_1,cars);
+        listview.setAdapter(adapter);
+
+        //////WHAT WILL HAPPEN IF ON OF THE LIST NAME IS CLICKED////
+        AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ///SHOWS A MEASSAGE IN LOG FILE///////////
+                Log.v(TAG,"*******You are at******"+position);
+            }
+        };
+        listview.setOnItemClickListener(onItemClickListener);
+
     }
 
     public void Click(View view){
